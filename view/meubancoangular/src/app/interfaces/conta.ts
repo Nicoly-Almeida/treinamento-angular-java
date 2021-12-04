ba4a70e227b4d56c7fd36f56aa9260a2394af0f5
Nicoly-Almeida <nicolyejady@gmail.com>
2021-12-04 14:21:47 -0300

    conta interface

--- view/meubancoangular/src/app/interfaces/conta.ts
+++ view/meubancoangular/src/app/interfaces/conta.ts
@@ -1,14 +1,9 @@
+import { Cliente } from "./cliente";
+
 export interface Conta {
-  "agencia": "string",
-  "cliente": {
-    "ativo": true,
-    "cpf": "string",
-    "email": "string",
-    "id": 0,
-    "nome": "string",
-    "observacoes": "string"
-  },
-  "id": 0,
-  "numero": "string",
-  "saldo": 0
+  agencia: "string",
+  cliente: Cliente,
+  id: 0,
+  numero: "string",
+  saldo: 0
 }
